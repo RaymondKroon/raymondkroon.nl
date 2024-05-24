34bbde23fe3499f1bcf5827eff717d59e0d6b2fb
Raymond Kroon <raymond@k3n.nl>
2024-05-24 13:20:49 +0200

    change background

--- src/lib/backgrounds/Stripes.svelte
+++ src/lib/backgrounds/Stripes.svelte
@@ -6,12 +6,15 @@
 	let totalLines = 0;
 	let linesWidth = 0;
 	let screenWidth = 0;
+	let documentHeight = 0;
 
 	function generateBg() {
 		screenWidth = window.innerWidth;
+		documentHeight = document.documentElement.scrollHeight;
 
-		linesWidth = Math.min(990, screenWidth - 50);
+		linesWidth = Math.min(990, screenWidth - 60);
 		totalLines = Math.floor(linesWidth / lineWidth);
+		totalLines = totalLines - (totalLines % 8) + 1;
 		linesWidth = totalLines * lineWidth;
 
 		let colors = [
@@ -50,8 +53,8 @@
 
 <svelte:window on:resize="{generateBg}" />
 
-<svg class="mx-auto" width="{linesWidth}" height="100">
+<svg class="mx-auto" width="{linesWidth}" height="{documentHeight}">
 	{#each lines as line, i}
-		<rect class={line.class} x={i * lineWidth} y="0" width={lineWidth} height="100" />
+		<rect class={line.class} x={i * lineWidth} y="0" width={lineWidth} height="{documentHeight}" />
 	{/each}
 </svg>
--- src/routes/(main)/+layout.svelte
+++ src/routes/(main)/+layout.svelte
@@ -3,7 +3,7 @@
 	import Footer from './Footer.svelte';
 </script>
 
-<div class="container flex flex-col max-w-screen-lg mx-auto p-10 rounded-2xl my-20s screen:bg-off-white screen:drop-shadow-[0_10px_10px_rgba(0,0,0,.7)]">
+<div class="container min-h-screen flex flex-col max-w-screen-lg mx-auto p-10 rounded-2xl my-20s screen:bg-off-white screen:drop-shadow-[0_10px_10px_rgba(0,0,0,.7)]">
 	<Header />
 	<slot></slot>
 	<Footer />
